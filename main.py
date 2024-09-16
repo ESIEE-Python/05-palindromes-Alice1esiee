3a661a6a5bacd31668c08a5fd879aa16ae37fdc4
Alice LIN <alice.lin@edu.esiee.fr>
2024-09-16 11:26:26 +0000

    fonction sur palindrome

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-#### Fonction secondaire
-
-
+#Fonction secondaire
+"""Fonction qui détermine si un mot est un palindrome."""
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """
+    >>>ispalindrome("radar")
+    True
+    >>>ispalindrome("civique")
+    False
+    >>>ispalindrome("kayak")
+    True
+    """
+    for i in range(0,(len(p)//2)):
+        if p[i] != p[-(i+1)]:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fonction qui contient une liste de mots et verifie avec la fonction 
+    ispalindrome si ce sont des palindromes.
 
-    # vos appels à la fonction secondaire ici
-
+    >>>main()
+    radar True
+    kayak True
+    level True
+    rotor True
+    civique False
+    deifie False
+    """
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
